0fb873caef95d79c7f06ba761e936d8a18722f57
mkentrru <mkentrru@yandex.ru>
2024-10-07 15:31:33 +0300

    remove pylib flag

--- top.mk
+++ top.mk
@@ -2,4 +2,4 @@ include $(_project_root)/.env
 
 PATH_NS3_STATIC_BUILD_DIR=$(HOME)/workspace/ns-allinone-3.42/ns-3.42/static-build
 
-PYBINDING_LIB_FLAG=-lpython3.11
+# PYBINDING_LIB_FLAG=-lpython3.11
